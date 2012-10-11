68f521b84fba35932063b590a2e1821b4c9ddee7
root <root@linux-ys7b.site>
2012-10-11 13:45:16 -0400

    add file

---
+++ ArrayDataManager.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "ArrayDataManager.h"
+
+ArrayDataManager::ArrayDataManager()
+{
+
+}
+ArrayDataManager::~ArrayDataManager()
+{
+
+}
+int ArrayDataManager::initial()
+{
+	printf("ArrayDataManager::initial\n");
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ArrayDataManager.h
@@ -0,0 +1,40 @@
+#ifndef ARRAYDATEMANAGER_H
+#define ARRAYDATEMANAGER_H
+
+class ArrayDataManager
+{
+	public:	
+	ArrayDataManager();
+	virtual ~ArrayDataManager();
+
+	int initial();
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
